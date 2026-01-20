be047c43de5e137e1360fae6a1476915719b2a30
Zio Gregory Magugat <zimagugat@gmail.com>
2026-01-20 18:45:15 +0800

    grid background dark mode adaptability added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       
       <main className="container mx-auto px-4 py-8">
         <div className="absolute inset-0 bg-white dark:bg-[#0c0c0c]">
-          <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f15_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f15_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+          <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f15_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f15_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#ffffff15_1px,transparent_1px),linear-gradient(to_bottom,#ffffff15_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
         </div>
 
         {/* Hero Section*/}
