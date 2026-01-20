c2acfe2bcc4755dc1e5adfafb8f2115b2b84a51c
Zio Gregory Magugat <zimagugat@gmail.com>
2026-01-20 18:15:48 +0800

    changed div to section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
     <div className="min-h-screen bg-[white] dark:bg-[#0c0c0c]">
       <Navbar />
       <main className="container mx-auto px-4 py-8">
-        <div className="flex flex-col justify-center items-center space-y-6 md:space-y-8">
+        {/* Hero */}
+        <section className="flex flex-col justify-center items-center space-y-6 md:space-y-8">
           <span className="inline-block mt-10 md:mt-20 px-3 py-1.5 md:px-4 md:py-2 text-sm md:text-base lg:text-lg font-medium bg-black dark:bg-white text-white dark:text-black rounded-full">Automated Structural Inspection Framework</span>
           <h1 className="text-4xl font-bold tracking-tighter max-w-2xl lg:max-w-5xl xl:max-w-6xl text-center sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl bg-gradient-to-r from-black via-gray-700 to-black bg-clip-text text-transparent dark:from-white dark:via-gray-300 dark:to-white px-4">Ensure safety with early concrete defect detection</h1>
           <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-normal text-gray-600 dark:text-gray-400 max-w-3xl lg:max-w-4xl xl:max-w-5xl text-center px-4">
@@ -25,9 +26,10 @@ export default function Home() {
               See How It Works
             </Button>
           </div>
-        </div>
-
-        <div className="mt-60 md:mt-72 lg:mt-80">
+        </section>
+        
+        {/* Get Started */}
+        <section className="mt-60 md:mt-72 lg:mt-80">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-center">
             {/* Left Side - Text */}
             <div className="text-center lg:text-left">
@@ -88,7 +90,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </div>
+        </section>
         
       </main>
     </div>
